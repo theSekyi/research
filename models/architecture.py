511fc13106f84cd19ac23dfba4161dfa23c817f0
theSekyi <emmanuelkwamesekyi@gmail.com>
2022-02-28 22:23:19 +0200

    commented CnnMed

--- models/architecture.py
+++ models/architecture.py
@@ -227,20 +227,20 @@ class CnnGalaxy(nn.Module):
 int_layer = 3380
 
 
-class CnnMed(nn.Module):
-    def __init__(self):
-        super(CnnMed, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(int_layer, 50)
-        self.fc2 = nn.Linear(50, 4)
+# class CnnMed(nn.Module):
+#     def __init__(self):
+#         super(CnnMed, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+#         self.conv2_drop = nn.Dropout2d()
+#         self.fc1 = nn.Linear(int_layer, 50)
+#         self.fc2 = nn.Linear(50, 4)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+#     def forward(self, x):
+#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
+#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+#         x = x.view(x.size(0), -1)
+#         x = F.relu(self.fc1(x))
+#         x = F.dropout(x, training=self.training)
+#         x = self.fc2(x)
+#         return F.log_softmax(x, dim=1)
